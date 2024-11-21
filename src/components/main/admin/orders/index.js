08884fbfe827c8c759fbf1833325ea4ef5dbf253
contentlywriters <faciocontentlywriters@gmail.com>
2024-11-21 23:06:27 +0530

    orders

--- src/components/main/admin/orders/index.js
+++ src/components/main/admin/orders/index.js
@@ -1,3 +1,4 @@
+"use client";
 import React, { useEffect, useState } from "react";
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Button } from "@/components/ui/button";

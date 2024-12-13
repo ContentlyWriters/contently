d28798e1133a353156f096b8367b3a8bd22cdd46
contentlywriters <faciocontentlywriters@gmail.com>
2024-12-13 16:20:15 +0530

    google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,10 @@
 import { Inter } from "next/font/google";
 import { AuthProvider } from "@/context/auth";
 import "./globals.css";
-import Script from 'next/script';
-import { ToastContainer, toast } from "react-toastify";
+import Script from "next/script";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -14,9 +15,25 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics Script */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-LMDV800CGT"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-LMDV800CGT');
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>
-        <AuthProvider>{children}
-          <Script src='https://checkout.razorpay.com/v1/checkout.js' />
+        <AuthProvider>
+          {children}
+          <Script src="https://checkout.razorpay.com/v1/checkout.js" />
         </AuthProvider>
         <ToastContainer />
       </body>

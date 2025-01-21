3c8a4561705887e36c8c6e7ca88e2673b6dffc0a
SIDHU <sidhu11111@gmail.com>
2025-01-21 16:21:33 +0530

    update...

--- src/components/main/popup/index.js
+++ src/components/main/popup/index.js
@@ -42,7 +42,7 @@ const Popup = () => {
   };
 
   const handleCopyCoupon = () => {
-    navigator.clipboard.writeText("SAVE20").then(() => {
+    navigator.clipboard.writeText("FIRST20").then(() => {
       setIsCopied(true);
       setTimeout(() => setIsCopied(false), 2000);
     });
@@ -123,14 +123,14 @@ const Popup = () => {
 {/* Show "Thanks for Joining" popup only if it hasn't been shown already */}
       {couponPopup && !localStorage.getItem("thanksPopupShown") && (
         <div className="fixed inset-0 bg-black bg-opacity-70 flex justify-center items-center z-50">
-        <div className="bg-[#e6f7ff] p-6 rounded-lg shadow-lg text-center max-w-sm w-full">
-          <h3 className="text-3xl font-extrabold text-blue-800">Thanks for Joining! 🎆</h3>
-          <p className="text-blue-600 mt-2 font-medium">Your coupon code is:</p>
+        <div className="bg-[#ffffff] border-2 border-black p-6 rounded-lg shadow-lg text-center max-w-sm w-full">
+          <h3 className="text-3xl font-extrabold text-[#5b6cf2]">Thanks for Joining! 🎆</h3>
+          <p className="text-black mt-2 font-medium">Your coupon code is:</p>
           <div
-            className="bg-gradient-to-r from-blue-600 to-silver-600 text-white text-xl font-bold py-2 px-4 rounded-md mt-4 cursor-pointer hover:from-silver-600 hover:to-blue-600 shadow-lg transform transition-transform"
+            className="bg-black  text-white text-xl font-bold py-2 px-4 rounded-md mt-4 cursor-pointer hover:border-b-4 hover:border-[#fff] border-b-4 border-[#5b6cf2] shadow-lg transform transition-transform"
             onClick={handleCopyCoupon}
           >
-            SAVE20
+            FIRST20
           </div>
           {isCopied && (
             <p className="text-blue-600 mt-2 text-sm font-semibold">Coupon code copied!</p>

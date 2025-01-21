1d68a5adeab397278f1d4440eda8fff4a7dc5717
SIDHU <sidhu11111@gmail.com>
2025-01-21 14:40:33 +0530

    google tag manager

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 import Script from "next/script";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { toast, Toaster } from "sonner";
+import {  Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,6 +30,19 @@ export default function RootLayout({ children }) {
             gtag('config', 'G-LMDV800CGT');
           `}
         </Script>
+
+          {/* Google Tag Manager Script */}
+          <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-N8KFBB92');
+          `}
+        </Script>
+
+         {/* Structured Data */}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
@@ -58,6 +71,17 @@ export default function RootLayout({ children }) {
         />
       </head>
       <body className={inter.className}>
+
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N8KFBB92"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <AuthProvider>
           {children}
           <Script src="https://checkout.razorpay.com/v1/checkout.js" />

237ef4605bf1cdcea4c4a66bd92f65c55d2fe4e3
SIDHU <sidharthmeena57539@gmail.com>
2025-10-22 14:38:56 +0530

    contentlywriters 2.O

--- src/components/main/auth/login/index.js
+++ src/components/main/auth/login/index.js
@@ -20,7 +20,8 @@ const { setIsLoggedIn } = useUserContext();
 
   const handleGoogleLogin = () => {
     const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
-    const redirectUri = "https://www.contentlywriters/api/auth/google/callback";
+  const redirectUri = "https://www.contentlywriters.com/api/auth/callback/google";
+
     const scope = "openid email profile";
 
     const url = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope}&prompt=consent`;
